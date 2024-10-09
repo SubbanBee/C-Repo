cf36b79372eb04ca54fe710f736c5b25e8b44508
SubbanBee <subbanbeeepanagandla@gmail.com>
2024-10-09 19:01:47 +1000

    Uploading code: Romeo and Juliet.c

---
+++ Romeo and Juliet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int X,Y,Z;
+    scanf("%d %d %d",&X,&Y,&Z);
+    int r=(5*X+10*Y)/Z;
+    scanf("%d",&r);
+    printf("%d",r);
+    return 0;
+
+}
